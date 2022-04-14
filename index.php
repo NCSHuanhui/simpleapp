616e4c3152747092b227fef2293f31357f74c1bf
NCSHuanhui <44329870+NCSHuanhui@users.noreply.github.com>
2022-04-14 15:56:20 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <html> 
 	<head> 
-	<title>PHP Test</title>
+	<title>PHP Test from Lab</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1"/>
 	</head>
 
 <body> 
-	<h1>PHP Test</h1>
+	<h1>PHP Test from Lab </h1>
 		<p><b>An Example of PHP in Action</b></p>
 		<?php echo "The Current Date and Time is: <br />"; 
 		echo date("g:i A l, F j Y.");?> </p>
